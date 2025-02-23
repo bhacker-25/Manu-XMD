bceb9378912f60b8009f91626d473fd9bfeef93d
bhacker-25 <indianofficialacverification@yahoo.com>
2025-02-23 17:59:40 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Manu-MD&tTM0DAyS#1w-OtN51XbmlxSLRAGBeg0H7p7kVrcRPA0azdXLAqhk' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Manu-MD&5e0wCLob#1TH9rMh1c0bPo8UsUJIjlRMIEygjFbIfzlpyCyPsoEo' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '919401105736' : process.env.SUDO,
